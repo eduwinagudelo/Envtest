5e1e14d497ddf7f5d2669700da101fdf7224de5b
Edwin Agudelo <eduwin.agudelo@gmail.com>
2024-09-04 20:21:17 -0500

    Using external variable

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern char **environ;
+
 int main()
 {
-    printf("Hello world!\n");
+    for (int i = 0; environ[i] != NULL; i++) {
+        printf("%d : %s\n", i, environ[i]);
+    }
     return 0;
 }
