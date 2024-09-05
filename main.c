7442dcd1bb2446a22b9af7694e3d884e2ae20b68
Edwin Agudelo <eduwin.agudelo@gmail.com>
2024-09-04 20:25:19 -0500

    envp add to main function

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern char **environ;
 
-int main()
+int main(int argc, char **argv, char **envp)
 {
-    for (int i = 0; environ[i] != NULL; i++) {
-        printf("%d : %s\n", i, environ[i]);
+    for (int i = 0; envp[i] != NULL; i++) {
+        printf("%d : %s\n", i, envp[i]);
     }
     return 0;
 }
